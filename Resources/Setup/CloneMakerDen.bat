908ea91d83bf43147c96744bc18bc566afa35137
gloveboxes <dglover.au@hotmail.com>
2015-11-02 17:13:33 +1100

    doco

--- Resources/Setup/CloneMakerDen.bat
+++ Resources/Setup/CloneMakerDen.bat
@@ -3,6 +3,7 @@ REM Getting the source code
 SET "SetupResources=c:\MakerDen\Resources\"
 SET "Solution=c:\MakerDen\Source\"
 
+rd /s /q %solution%
 
 git clone https://github.com/MakerDen/IoT-Maker-Den-Windows-for-IoT.git --depth 1 %Solution%
 
@@ -17,6 +18,4 @@ cd %Solution%
 
 copy %SetupResources%VisualStudio\launcher.csproj.user.RPiName %Solution%Launcher\launcher.csproj.user /y
 
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe" /resetsettings %SetupResources%\VisualStudio\MakerDenSettings.vssettings %Solution%MakerDenWindows.sln
-
-
+start "Start Visual Studio 2015" /d "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\" /b  "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"  /resetsettings %SetupResources%\VisualStudio\MakerDenSettings.vssettings %Solution%MakerDenWindows.sln
