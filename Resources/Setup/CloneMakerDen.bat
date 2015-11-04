7ca51dc36bc1e16fdae4f06bfb5ab645033dd45f
gloveboxes <dglover.au@hotmail.com>
2015-11-04 12:18:37 +1100

    doco

--- Resources/Setup/CloneMakerDen.bat
+++ Resources/Setup/CloneMakerDen.bat
@@ -13,12 +13,13 @@ REM copy across the snippets
 CALL %SetupResources%\Setup\Snippets
 
 REM Copy across Desktop shortcuts
-xcopy "%SetupResources%Shortcuts\*.*"  "%userprofile%\desktop" /s
+xcopy "%SetupResources%Shortcuts"  "%userprofile%\desktop" /s /i
 
 cd %Solution%
 
+REM Copy Maker Den specific remote device config name
+copy %SetupResources%VisualStudio\launcher.csproj.user.RPiName %Solution%Launcher\launcher.csproj.user /y
 
-copy %SetupResources%VisualStudio\launcher.csproj.user.RPiName %Solution%Launcher\launcher.csproj.user /y /s /i
 
 start "Start Visual Studio 2015" /d "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\" /b  "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\devenv.exe"  /resetsettings %SetupResources%\VisualStudio\MakerDenSettings.vssettings %Solution%MakerDenWindows.sln
 
